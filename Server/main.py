aa73dde8a50633bb8af60af9bbe728e4d68c8048
8itwise <supremebigboy@gmail.com>
2023-06-18 13:29:22 +0100

    Completed Prototype

--- Server/main.py
+++ Server/main.py
@@ -1,3 +1,8 @@
+#Created by Xand
+
+#Disclaimer: This reverse shell should only be used in the lawful, remote administration of authorized systems. Accessing a computer network without authorization or permission is illegal.
+
+
 from caesar import Caesar
 import os
 
